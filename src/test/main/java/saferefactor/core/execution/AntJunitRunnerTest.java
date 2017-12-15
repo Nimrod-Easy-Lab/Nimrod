0b4872e4b5b95305a2ac4550fa921fd60d9652ae
pmop <pmop@ic.ufal.br>
2017-12-15 10:43:24 -0300

    Revert "Added missing files required by tests and removed maven exec plugin"
    This reverts commit 172576e950a26dda04a2406ed24b5061458578a2.
    
    mvn package failed. Reverting commits.

--- src/test/main/java/saferefactor/core/execution/AntJunitRunnerTest.java
+++ src/test/main/java/saferefactor/core/execution/AntJunitRunnerTest.java
@@ -16,12 +16,12 @@ public class AntJunitRunnerTest {
 	@Test
 	public void test() throws FileNotFoundException {
 		Project project= new Project();
-		project.setProjectFolder(new File("src/test/subjects/subject14source"));
-		project.setBuildFolder(new File("src/test/subjects/subject14source/bin"));
-		project.setSrcFolder(new File("src/test/subjects/subject14source/src"));
+		project.setProjectFolder(new File("test/subjects/subject14source"));
+		project.setBuildFolder(new File("test/subjects/subject14source/bin"));
+		project.setSrcFolder(new File("test/subjects/subject14source/src"));
 		
-		String reportPath = "src/test/subjects/subject14source/report";
-		String tests = "src/test/subjects/subject14source/test";
+		String reportPath = "test/subjects/subject14source/report";
+		String tests = "test/subjects/subject14source/test";
 		TestExecutor testExecutor = new AntJunitRunner(project,tests);
 		testExecutor.setReportPath(reportPath);
 		testExecutor.executeTests(tests);
